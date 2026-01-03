b9b9b8262a63fceecb33657cd564706ff15103d6
Lukas Rauscher <lukas@rauscher.ai>
2026-01-03 19:21:11 +0100

    feat: integrate Tailwind CSS and DaisyUI
    - update default app.vue for future page rendering
    - add base config for daisyui to main.css

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtRouteAnnouncer/>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite"
 
-  modules: [
-    '@nuxt/fonts',
-    '@nuxt/hints',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils'
-  ]
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+
+    modules: [
+        '@nuxt/fonts',
+        '@nuxt/hints',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        '@nuxt/test-utils'
+    ],
+
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
 })
