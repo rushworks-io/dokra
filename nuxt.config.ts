3761534d0df47a0a602d2288cb5ef9aa91addf3e
Lukas Rauscher <lukas@rauscher.ai>
2026-01-03 19:12:54 +0100

    initialize Nuxt.js project with essential configuration and some basic official modules

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ]
+})
